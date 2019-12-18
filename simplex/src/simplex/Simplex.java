0e1459f78cbc8090f4b73bb03641ea60b7c2206a
rikTorres <riky.paramore@gmail.com>
2019-12-17 20:14:26 -0500

    simplex
    Actualizacion

--- simplex/src/simplex/Simplex.java
+++ simplex/src/simplex/Simplex.java
@@ -1,11 +1,12 @@
 package simplex;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import javax.swing.JOptionPane;
 
 public class Simplex {
     public static ArrayList<funciones> Listfunciones = new ArrayList();
-    
+    public static ArrayList<Double> terminoPivot = new ArrayList();
     public static void main(String[] args) {
         int numVariables = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce el numero de variables"));
         int tipo = JOptionPane.showOptionDialog(null, "Seleccione el operador",
@@ -18,6 +19,7 @@ public class Simplex {
         for (int i = 0; i < numVariables; i++) {
             funcionZ.incognitas.add(Double.parseDouble(JOptionPane.showInputDialog(null, "Introduce el valor de X"+(i+1))));
         }
+        funcionZ.terminoIndependiente = (Double.parseDouble(JOptionPane.showInputDialog(null, "Introduce el valor del termino Independiente")));
         Listfunciones.add(funcionZ);
 
         
@@ -87,7 +89,7 @@ public class Simplex {
                Listfunciones.get(0).incognitas.set(i, Listfunciones.get(0).incognitas.get(i) * -1);
            }
        }
-       Listfunciones.get(0).terminoIndependiente=0.0;
+       //Listfunciones.get(0).terminoIndependiente=0.0;
        Listfunciones.get(0).condicion=1;
     }
     
@@ -136,13 +138,15 @@ public class Simplex {
         }
     }
     
+   
     
     public static void pivotear(){
+        terminoPivot.clear();
         Double a=0d;
         int pivote = 0;
         Double div=0d;
         int func = 0;
-        for (int i = 0; i < Listfunciones.get(0).incognitas.size()-1; i++) {
+        for (int i = 0; i < Listfunciones.get(0).incognitas.size(); i++) {
             if(Listfunciones.get(0).incognitas.get(i) < a){
                 a = Listfunciones.get(0).incognitas.get(i);
                 pivote = i;
@@ -152,54 +156,66 @@ public class Simplex {
         if(a != 0){
         System.out.println("Termino Pivote: "+a+" indice pivote: "+pivote);
         a=1000d;
-        for (int i = 1; i < Listfunciones.size(); i++) {
-            if(Listfunciones.get(i).incognitas.get(pivote) >= 0){
-                div = Listfunciones.get(i).terminoIndependiente/Listfunciones.get(i).incognitas.get(pivote);
-                if(div < a){
-                    a = div;
-                    func = i;
+        //ENCONTRAR RAZON
+            for (int i = 1; i < Listfunciones.size(); i++) {
+                if(Listfunciones.get(i).incognitas.get(pivote) >= 0){
+                    div = Listfunciones.get(i).terminoIndependiente/Listfunciones.get(i).incognitas.get(pivote);
+                    if(div < a){
+                        a = div;
+                        func = i;
+                    }
                 }
+
             }
+            System.out.println("razon de la ecuacion "+func+" es: "+a);
             
-        }
-        System.out.println("razon de la ecuacion "+func+" es: "+a);
-        //MULTIPLICAR PARA ELIMINAR Y DEJAR EN TERMINOS DE 1
+            //MULTIPLICAR Y ELIMINAR
             for (int i = 0; i < Listfunciones.size(); i++) {
                 Double terminoPivo = Listfunciones.get(i).incognitas.get(pivote);
-                 if(Listfunciones.get(i).incognitas.get(pivote) > 0){
-                     for (int j = 0; j < Listfunciones.get(i).incognitas.size(); j++) {
-                         Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)/Math.abs(terminoPivo));
-                            if(i != func){
-                                Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)+(Listfunciones.get(func).incognitas.get(j) * -1));
-                                Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)* Math.abs(terminoPivo));
-                            }
-                    }
-                 }else{
-                     for (int j = 0; j < Listfunciones.get(i).incognitas.size(); j++) {
-                         Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)/Math.abs(terminoPivo));
-                            if(i != func){
-                                Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)+(Listfunciones.get(func).incognitas.get(j)));
-                                Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)* Math.abs(terminoPivo));
-                            }
-                     }
-                 }
-                Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente / Math.abs(terminoPivo);
-                if(i != func){
-                    if(Listfunciones.get(i).incognitas.get(pivote) > 0){
-                        Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente + (Listfunciones.get(func).terminoIndependiente * -1);
-                        Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente *  Math.abs(terminoPivo);
-                    }else{
-                        Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente + (Listfunciones.get(func).terminoIndependiente);
-                        Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente *  Math.abs(terminoPivo);
-                    }
-                }
+                if(terminoPivo != 0){
+                        for (int j = 0; j < Listfunciones.get(i).incognitas.size(); j++) {
+                            Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)/Math.abs(terminoPivo));
+                        }
+                        Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente / Math.abs(terminoPivo);
+                        terminoPivot.add(Math.abs(terminoPivo));
+                }else{
+                    terminoPivot.add(1.0);
+                }  
             }
-        
-        presentarTabla();
-        
+            
+            multiplicar(func, pivote);
+            //pivotear2();
         }else{
             System.out.println("Programa Termino");
         }
+    
     }
     
+    public static void multiplicar(int func, int pivote){
+        for (int i = 0; i < Listfunciones.size(); i++) {            
+            if(Listfunciones.get(i).incognitas.get(pivote) != 0){
+                if(Listfunciones.get(i).incognitas.get(pivote) > 0){
+                    if(i != func){
+                        for (int j = 0; j < Listfunciones.get(i).incognitas.size(); j++) {
+                            Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)+(Listfunciones.get(func).incognitas.get(j) * -1));
+                            Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)* terminoPivot.get(i));
+                        }
+                        Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente + (Listfunciones.get(func).terminoIndependiente * -1) ;
+                        Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente * terminoPivot.get(i);
+                    }
+                }else{
+                    if(i != func){
+                        for (int j = 0; j < Listfunciones.get(i).incognitas.size(); j++) {
+                            Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)+(Listfunciones.get(func).incognitas.get(j)));
+                            Listfunciones.get(i).incognitas.set(j,Listfunciones.get(i).incognitas.get(j)* terminoPivot.get(i));
+                        }
+                        Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente + (Listfunciones.get(func).terminoIndependiente) ;
+                        Listfunciones.get(i).terminoIndependiente = Listfunciones.get(i).terminoIndependiente * terminoPivot.get(i);
+                    }
+                }
+            }
+        }
+        presentarTabla();
+        pivotear();
+    }
 }
